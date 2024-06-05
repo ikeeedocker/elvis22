05341be9637347c21779d5a497102fc1277d4d5c
ikeeedocker <ikeee.docker@gmail.com>
2024-06-05 17:23:21 +0200

    Create due.sh

---
+++ due.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Directory da aggiungere al PATH
+NEW_DIR="/usr/local/go/bin"
+
+# Verifica se la directory è già nel PATH
+if [[ ":$PATH:" != *":$NEW_DIR:"* ]]; then
+  # Aggiungi la directory al .bashrc se non è già presente
+  echo "export PATH=\"\$PATH:$NEW_DIR\"" >> ~/.bashrc
+  echo "La directory $NEW_DIR è stata aggiunta al PATH."
+else
+  echo "La directory $NEW_DIR è già presente nel PATH."
+fi
+go version
