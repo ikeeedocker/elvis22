aef683f769dc7123f9b461f93d282eb6763d682b
ikeeedocker <ikeee.docker@gmail.com>
2024-06-20 10:57:34 +0200

    Update confirm.sh

--- confirm.sh
+++ confirm.sh
@@ -1,6 +1,6 @@
 confirm() {
     # call with a prompt string or use a default
-    read -r -p "${1:-Are you sure? [y/N]} " response
+    read -r -p "${1:-Are you sure you want to continue? [y/n]:} " response
     case "$response" in
         [yY][eE][sS]|[yY]) 
             true
